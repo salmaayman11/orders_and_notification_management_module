20e1f011cad7e29a137ea9f18dd63c04cf9be26f
mariam0ashraf <mariam00ashraf@gmail.com>
2023-12-31 03:08:22 +0200

    all done->

--- src/main/java/com/example/demo/model/notification/MsgQueue.java
+++ src/main/java/com/example/demo/model/notification/MsgQueue.java
@@ -1,6 +1,7 @@
 package com.example.demo.model.notification;
 import java.util.Queue;
 import java.util.LinkedList;
+import java.util.concurrent.*;
 public class MsgQueue {
     private Queue<Notification> msgs = new LinkedList<>();
 
@@ -13,10 +14,19 @@ public class MsgQueue {
         }
         return null;
     }
-    public Notification pop() {
-        if (!notificationQueue.isEmpty()) {
-            return notificationQueue.pop();
+    public void pop() {
+        if (!msgs.isEmpty()) {
+            ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+            executorService.scheduleAtFixedRate(() -> {
+                try {
+                     msgs.poll();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }, 0, 30, TimeUnit.SECONDS);
+            executorService.shutdown();
         }
-        return null;
+        }
+
     }
-}
+
